3bc584a221fcdd679dab88bdd8287be3fdd52655
Ryan Brown <ryansb@csh.rit.edu>
2013-04-06 11:56:36 -0400

    Add a basic user service

--- panstora/views.py
+++ panstora/views.py
@@ -2,6 +2,8 @@
 """
 from cornice import Service
 
+from panstora.models import User
+
 
 hello = Service(name='hello', path='/', description="Simplest app")
 
@@ -10,3 +12,9 @@ hello = Service(name='hello', path='/', description="Simplest app")
 def get_info(request):
     """Returns Hello in JSON."""
     return {'Hello': 'World'}
+
+users = Service('users', path='/users', description="Provides user info")
+
+@users.get()
+def get_users(request):
+    return [u.to_dict() for u in User.all()]
