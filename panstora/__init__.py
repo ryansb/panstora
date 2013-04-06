fd0db57176a017b0d86b837e0cde18cb1ad1a4c3
David Gay <oddshocks@riseup.net>
2013-04-06 12:26:25 -0400

    prepare for home view and static view, add empty css file

--- panstora/__init__.py
+++ panstora/__init__.py
@@ -5,6 +5,12 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-    config.include("cornice")
     config.scan("panstora.views")
+
+    # Static view setup
+    config.add_static_view('static', 'static', cache_max_age=3600)
+
+    # Set up routes
+    config.add_route('home', '/')
+
     return config.make_wsgi_app()
